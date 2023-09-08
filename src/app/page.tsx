fdb2f527be7ab2e105b248772697dded73b00ff2
Drew Cook <drew@dco.dev>
2023-09-08 14:07:32 -0600

    chore: lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper, Typography } from '@mui/material'
+import { Paper, Typography } from '@mui/material'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
