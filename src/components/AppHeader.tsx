9dcd2e55ce52f9db2490b1bfb98216cf5f260654
Drew Cook <drew@dco.dev>
2023-11-10 22:52:51 -0700

    fix: update app header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -21,18 +21,16 @@ import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi'
 
 import formatAddress from '@/utils/formatAddress'
 
-// import Link from 'next/link'
 import ConnectWalletButton from './ConnectWalletButton'
 
 const styles = {
 	appBar: { backgroundColor: grey[900] },
-	navigationMobileWrap: { flexGrow: 0, display: { xs: 'flex', md: 'none' }, mr: 1 },
+	navigationMobileWrap: { display: { xs: 'flex', md: 'none' }, flexGrow: 1, alignItems: 'center', mr: 1 },
 	navigationMobileMenu: { display: { xs: 'block', md: 'none' } },
-	navigationDesktopWrap: { flexGrow: 1, display: { xs: 'none', md: 'flex' } },
+	navigationDesktopWrap: { display: { xs: 'none', md: 'flex' }, flexGrow: 1, alignItems: 'center' },
 	logoMobile: {
-		mr: 2,
+		mx: 2,
 		display: { xs: 'flex', md: 'none' },
-		flexGrow: 1,
 		fontFamily: 'monospace',
 		fontWeight: 700,
 		letterSpacing: '.3rem',
@@ -51,11 +49,14 @@ const styles = {
 	navigationLink: { my: 2, color: 'white', display: 'block' },
 	userConnectedButton: { px: 2, py: 0.75 },
 	userAvatar: { ml: 1, width: '24px', height: '24px', flexGrow: 0, fontSize: '12px' },
+	userMenuWrap: { flexGrow: 0 },
 	userMenu: { mt: '45px' },
 }
 
 const AppHeader = () => {
+	// App Title
 	const dappTitleText = 'WEB3DAPP'
+
 	// Navigation Pages
 	const pages = [
 		{
@@ -63,10 +64,12 @@ const AppHeader = () => {
 			href: '/dashboard',
 		},
 	]
-	const settings = ['Switch Network', 'Switch Wallet', 'Disconnect']
+	const userMenuItems = ['Switch Network', 'Switch Wallet', 'Disconnect']
+
 	// State
 	const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
 	const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
+
 	// Hooks
 	const { address, isConnected } = useAccount()
 	const { disconnect } = useDisconnect()
@@ -74,6 +77,7 @@ const AppHeader = () => {
 	const { data: ensAvatar } = useEnsAvatar({ name: ensName })
 	const { open } = useWeb3Modal()
 
+	// Handlers
 	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
 		setAnchorElNav(event.currentTarget)
 	}
@@ -92,6 +96,15 @@ const AppHeader = () => {
 		setAnchorElUser(null)
 	}
 
+	// Components
+	const MenuNavigationItems = pages.map(page => (
+		<Link key={page.text} href={page.href}>
+			<MenuItem onClick={handleCloseNavMenu}>
+				<Typography textAlign="center">{page.text}</Typography>
+			</MenuItem>
+		</Link>
+	))
+
 	return (
 		<AppBar position="static" elevation={0} sx={styles.appBar}>
 			<Container maxWidth="xl">
@@ -109,6 +122,7 @@ const AppHeader = () => {
 							<MenuIcon />
 						</IconButton>
 						<Menu
+							sx={styles.navigationMobileMenu}
 							id="menu-appbar"
 							anchorEl={anchorElNav}
 							anchorOrigin={{
@@ -122,72 +136,63 @@ const AppHeader = () => {
 							}}
 							open={Boolean(anchorElNav)}
 							onClose={handleCloseNavMenu}
-							sx={styles.navigationMobileMenu}
 						>
-							{pages.map(page => (
-								<Link key={page.text} href={page.href}>
-									<MenuItem onClick={handleCloseNavMenu}>
-										<Typography textAlign="center">{page.text}</Typography>
-									</MenuItem>
-								</Link>
-							))}
+							{MenuNavigationItems}
 						</Menu>
+						<Link href="/">
+							<Typography variant="h5" noWrap sx={styles.logoMobile}>
+								{dappTitleText}
+							</Typography>
+						</Link>
 					</Box>
-
-					{/* Logo */}
-					<Typography variant="h6" noWrap component="a" href="/" sx={styles.logoDesktop}>
-						{dappTitleText}
-					</Typography>
-					<Typography variant="h5" noWrap component="a" href="/" sx={styles.logoMobile}>
-						{dappTitleText}
-					</Typography>
 
 					{/* Desktop Navigation */}
 					<Box sx={styles.navigationDesktopWrap}>
-						{pages.map(page => (
-							<Link key={page.text} href={page.href}>
-								<Button onClick={handleCloseNavMenu} sx={styles.navigationLink}>
-									{page.text}
-								</Button>
-							</Link>
-						))}
+						<Link href="/">
+							<Typography variant="h5" noWrap sx={styles.logoDesktop}>
+								{dappTitleText}
+							</Typography>
+						</Link>
+						{MenuNavigationItems}
 					</Box>
 
-					{/* Connected User Menu */}
-					{isConnected ? (
-						<>
-							<Tooltip title="Open settings">
-								<Button variant="outlined" onClick={handleOpenUserMenu} sx={styles.userConnectedButton}>
-									<Typography variant="caption">{ensName || formatAddress(address)}</Typography>
-									<Avatar alt="User ENS Avatar" src={`${ensAvatar}`} sx={styles.userAvatar} />
-								</Button>
-							</Tooltip>
-							<Menu
-								sx={styles.userMenu}
-								id="menu-appbar"
-								anchorEl={anchorElUser}
-								anchorOrigin={{
-									vertical: 'top',
-									horizontal: 'right',
-								}}
-								keepMounted
-								transformOrigin={{
-									vertical: 'top',
-									horizontal: 'right',
-								}}
-								open={Boolean(anchorElUser)}
-								onClose={handleCloseUserMenu}
-							>
-								{settings.map(setting => (
-									<MenuItem key={setting} onClick={() => handleCloseUserMenu(setting)}>
-										<Typography textAlign="center">{setting}</Typography>
-									</MenuItem>
-								))}
-							</Menu>
-						</>
-					) : (
-						<ConnectWalletButton />
-					)}
+					{/* User Menu */}
+					<Box sx={styles.userMenuWrap}>
+						{isConnected ? (
+							<>
+								<Tooltip title="Open settings">
+									<Button variant="outlined" onClick={handleOpenUserMenu} sx={styles.userConnectedButton}>
+										<Typography variant="caption">{ensName || formatAddress(address)}</Typography>
+										<Avatar alt="User ENS Avatar" src={`${ensAvatar}`} sx={styles.userAvatar} />
+									</Button>
+								</Tooltip>
+								<Menu
+									sx={styles.userMenu}
+									id="menu-appbar"
+									anchorEl={anchorElUser}
+									anchorOrigin={{
+										vertical: 'top',
+										horizontal: 'right',
+									}}
+									keepMounted
+									transformOrigin={{
+										vertical: 'top',
+										horizontal: 'right',
+									}}
+									open={Boolean(anchorElUser)}
+									onClose={handleCloseUserMenu}
+								>
+									{userMenuItems.map(item => (
+										<MenuItem key={item} onClick={() => handleCloseUserMenu(item)}>
+											<Typography textAlign="center">{item}</Typography>
+										</MenuItem>
+									))}
+								</Menu>
+							</>
+						) : (
+							<ConnectWalletButton />
+						)}
+					</Box>
 				</Toolbar>
 			</Container>
 		</AppBar>
